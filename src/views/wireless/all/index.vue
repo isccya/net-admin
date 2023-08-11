581fd7cdfc1b54d0fec135f99e3cc224936dd17c
cc <2798750836@qq.com>
2023-08-11 17:04:05 +0800

    feat:all搜索1.0

--- src/views/wireless/all/index.vue
+++ src/views/wireless/all/index.vue
@@ -9,26 +9,26 @@ const queryParams = reactive({
 // ap树
 const apTree = reactive([
    {
-      Building: 'dormBuilding',
-      children: reactive([]),
+      Building: '宿舍区',
+      children: [],
    },
    {
-      Building: 'officeBuilding',
-      children: reactive([]),
+      Building: '办公区',
+      children: [],
    },
    {
-      Building: 'teachBuilding',
-      children: reactive([]),
+      Building: '教学区',
+      children: [],
    },
 ]);
 // ap详情
 let apDetail: any = reactive({})
 
-
+const apShow = ref(true)//控制哪个ap树展示
 const loading = ref(true); // 控制列表加载
 const showSearch = ref(true); //是否展示搜索区域
 const isExpandAll = ref(false);//是否展开所有数据
-const dialogVisible = ref(false) //H3Cap详细信息弹出框
+const dialogVisible = ref(false) //ap详细信息弹出框
 const judgeState = [{ value: null, label: '全部' }, { value: '1', label: '正常' }, { value: '2', label: '故障' }]
 
 // 获取ap树
@@ -38,11 +38,12 @@ function queryApTree() {
       if (res.code === 200) {
          apTree[0].children = res.data.dormBuilding;
          apTree[1].children = res.data.officeBuilding;
+         apTree[2].children = res.data.teachBuilding;
       }
       // 添加hasChildren属性
       function traverse(building) {
          if (!building.children || building.children.length === 0) {
-            if (building.Building !== 'dormBuilding' && building.Building !== 'officeBuilding' && building.Building !== 'teachBuilding') {
+            if (building.Building !== '宿舍区' && building.Building !== '办公区' && building.Building !== '教学区') {
                Object.defineProperty(building, "hasChildren", {
                   value: true,
                })
@@ -69,6 +70,9 @@ function load(row, treeNode, resolve) {
       if (!building.children || building.children.length === 0) {
          getApList(building.Building, queryParams).then((res: any) => {
             if (res.code === 200) {
+               if (res.data === null) {
+                  res.data = [];
+               }
                resolve(res.data)
             }
          })
@@ -107,6 +111,7 @@ function formateTime(second) {
 
 /** 搜索按钮操作 */
 function handleQuery() {
+   apShow.value = !apShow.value;
    queryApTree();
 }
 /** 重置按钮操作 */
@@ -124,7 +129,7 @@ onMounted(() => {
 <template>
    <div class="app-container">
       <!-- 搜索 -->
-      <!-- <el-form :model="queryParams" :inline="true" v-show="showSearch">
+      <el-form :model="queryParams" :inline="true" v-show="showSearch">
          <el-form-item label="AP名称" prop="apName">
             <el-input v-model="queryParams.apName" placeholder="请输入AP名称" clearable style="width: 200px"
                @keyup.enter="handleQuery" />
@@ -141,15 +146,14 @@ onMounted(() => {
       </el-form>
 
       <el-row :gutter="10" class="mb-8">
+         <right-toolbar v-model:showSearch="showSearch" @queryTable="queryApTree"></right-toolbar>
+      </el-row>
 
-         <right-toolbar v-model:showSearch="showSearch" @queryTable="queryApTree"></right-toolbar>  隐藏搜索和刷新按钮
-      </el-row> -->
-
-      <el-table v-loading="loading" :data="apTree" row-key="Building" :default-expand-all="isExpandAll" lazy
+      <el-table v-if="!apShow" v-loading="loading" :data="apTree" row-key="Building" :default-expand-all="isExpandAll" lazy
          :tree-props="{ children: 'children', hasChildren: 'hasChildren' }" :load="load">
          <!-- row-key是行数据的key,优化表格的渲染 -->
-         <el-table-column prop="Building" label="AP地址" width="200" align="center" />
-         <el-table-column prop="status" label="AP状态"  align="center">
+         <el-table-column prop="Building" label="AP地址" width="220" align="left" />
+         <el-table-column prop="status" label="AP状态" align="center">
             <template #default="scope">
                <div v-if="scope.row.commonState === 1">
                   <el-tag type="success">正常</el-tag>
@@ -159,11 +163,49 @@ onMounted(() => {
                </div>
             </template>
          </el-table-column>
-         <el-table-column prop="apName" label="AP名称" align="center" width="300"/>
-         <el-table-column prop="apModel" label="AP型号" align="center" width="180"/>
-         <el-table-column prop="mac" label="Mac地址" align="center" width="200"/>
+         <el-table-column prop="apName" label="AP名称" align="center" width="300" />
+         <el-table-column prop="apModel" label="AP型号" align="center" width="180" />
+         <el-table-column prop="mac" label="Mac地址" align="center" width="200" />
          <el-table-column prop="ip" label="IP地址" align="center" width="200" />
-         <el-table-column prop="name" label="平台类型" align="center"  width="150">
+         <el-table-column prop="name" label="平台类型" align="center" width="150">
+            <template #default="scope">
+               <div v-if="scope.row.platform === 0">
+                  H3C平台
+               </div>
+               <div v-if="scope.row.platform === 1">
+                  HuaWei平台
+               </div>
+            </template>
+         </el-table-column>
+         <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+            <template #default="scope">
+               <div v-if="scope.row.apName">
+                  <!-- <el-button link type="primary" icon="Edit" @click="console.log(123)">查看</el-button> -->
+                  <el-button type="primary" plain @click="queryApDetail(scope.row.uuid)">查看</el-button>
+               </div>
+            </template>
+         </el-table-column>
+      </el-table>
+
+      <el-table v-if="apShow" v-loading="loading" :data="apTree" row-key="Building" :default-expand-all="isExpandAll" lazy
+         :tree-props="{ children: 'children', hasChildren: 'hasChildren' }" :load="load">
+         <!-- row-key是行数据的key,优化表格的渲染 -->
+         <el-table-column prop="Building" label="AP地址" width="220"   align="left"/>
+         <el-table-column prop="status" label="AP状态" align="center">
+            <template #default="scope">
+               <div v-if="scope.row.commonState === 1">
+                  <el-tag type="success">正常</el-tag>
+               </div>
+               <div v-if="scope.row.commonState === 2">
+                  <el-tag type="danger">故障</el-tag>
+               </div>
+            </template>
+         </el-table-column>
+         <el-table-column prop="apName" label="AP名称" align="center" width="300" />
+         <el-table-column prop="apModel" label="AP型号" align="center" width="180" />
+         <el-table-column prop="mac" label="Mac地址" align="center" width="200" />
+         <el-table-column prop="ip" label="IP地址" align="center" width="200" />
+         <el-table-column prop="name" label="平台类型" align="center" width="150">
             <template #default="scope">
                <div v-if="scope.row.platform === 0">
                   H3C平台
